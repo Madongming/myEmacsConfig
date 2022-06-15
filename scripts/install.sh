28aa3bd82adfcb25298ce83447ba5a90a9af38e1
Madongming <elsove812@gmail.com>
2022-06-15 16:47:16 +0800

    use yarn

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,11 @@
 go install golang.org/x/tools/gopls@latest
 
 # install js, ts lsp
-npm i -g vscode-langservers-extracted
-npm i -g typescript-language-server typescript
+yarn global add vscode-langservers-extracted
+yarn global add typescript-language-server typescript
 
 # install python lsp
 pip3 install python-lsp-server
 
 # install solidity lsp
-npm i -g solidity-language-server
+yarn global add solidity-language-server
